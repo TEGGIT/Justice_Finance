5a14e2b01c0db36fc1b6aa7438aa636d78226aa5
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-05-13 13:49:03 +0300

    MainPage finish 90%

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -6,19 +6,24 @@ import classes from './MainPage.module.scss'
 
 import background from '../../assets/image/background.png'
 import starOne from '../../assets/image/StarOne.svg'
+import interfaceFinance from '../../assets/image/interface.png'
+import starTwo from '../../assets/image/StarTwo.svg'
+
 const MainPage = () => {
   return (
       <div className={classes.background} style={{backgroundImage: `url(${background})`}}>
       <Header/>
-
           <main className={classes.main}>
               <div className={classes.ellipseOne}/>
               <div className={classes.starOne} style={{backgroundImage:`url(${starOne})`}}/>
+              <div className={classes.ellipseTwo}/>
+              <div className={classes.starTwo} style={{backgroundImage:`url(${starTwo})`}}></div>
               <section className={classes.main__section}>
-                  <h1 className={classes.title}>Обменивайте валюту по выгодному курсу</h1>
-
+                  <div className={classes.main__section__title}>
+                      <h1 className={classes.title}>Обменивайте валюту по выгодному курсу</h1>
+                  </div>
                   <div className={classes.main__section__content}>
-ffff
+                      <img src={interfaceFinance} alt='interface'/>
                   </div>
 
               </section>
