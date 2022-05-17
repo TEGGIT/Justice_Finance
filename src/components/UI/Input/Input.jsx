c2b1a05c81289243f8ed3fbdac55d84747b2474c
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-05-17 12:43:34 +0300

    Create Profile page

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Input.module.scss'
-const Input = ({placeholder, value, styles, type, onChange}) => {
+const Input = ({placeholder, value, styles, type, onChange, size}) => {
   return (
     <>
           <input placeholder={placeholder}
                  value={value}
                  className={styles}
                  type={type}
-                 onChange={onChange}/>
+                 onChange={onChange}
+          />
     </>
 
 
