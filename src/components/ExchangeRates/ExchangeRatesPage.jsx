9759a02a36b01e1bdcf9a88450ee3be3c5943cd5
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-05-19 11:13:39 +0300

    Create chart page

--- src/components/ExchangeRates/ExchangeRatesPage.jsx
+++ src/components/ExchangeRates/ExchangeRatesPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import classes from './ExchangeRatesPage.module.scss'
 import NavBar from '../NavBar/NavBar'
 import Input from "../UI/Input/Input";
@@ -6,34 +6,62 @@ import ProfileBar from "../ProfileBar/ProfileBar";
 import SlideButton from "../UI/SlideButton/SlideButton";
 import arrowButtonLeft from '../../assets/image/ArrowButtonLeft.svg'
 import arrowButtonRight from '../../assets/image/arrowButtonRight.svg'
-import arrowUp from '../../assets/image/ArrowUp.svg'
+import arrowUpMin from '../../assets/image/ArrowUpMin.svg'
 import Charts from "./Chart/Chart";
 import SliderRate from "./SliderRate/SliderRate";
+import ButtonMui from "../MUI/Button/ButtonMui";
 
 const ExchangeRatesPage = () => {
   return (
-      <>
-          <div className={classes.wrapper}>
-            <NavBar/>
-            <main className={classes.main}>
-              <div className={classes.main_title}>
-                <h1 className={classes.main_title_text}>
-                  Курсы валют
-                </h1>
-                <Input styles={classes.main_title_input} placeholder="Поиск валюты"/>
-              </div>
-              <div className={classes.main_wrapper__slider}>
-                <SlideButton img={arrowButtonLeft}/>
-                 <SliderRate/>
-                  <SliderRate/>
-                <SliderRate/>
-                <SlideButton img={arrowButtonRight}/>
-              </div>
-              <Charts/>
-            </main>
-            <ProfileBar/>
+    <>
+      <div className={classes.wrapper}>
+        <NavBar/>
+        <main className={classes.main}>
+          <div className={classes.main_title}>
+            <h1 className={classes.main_title_text}>
+              Курсы валют
+            </h1>
+            <Input styles={classes.main_title_input} placeholder="Поиск валюты"/>
           </div>
-      </>
+          <div className={classes.main_wrapper__slider}>
+            <SlideButton img={arrowButtonLeft}/>
+            <SliderRate/>
+            <SliderRate/>
+            <SliderRate/>
+            <SliderRate/>
+            <SlideButton img={arrowButtonRight}/>
+          </div>
+          <div className={classes.main_wrapper__chart__title}>
+            <div className={classes.main_wrapper__chart__title_text}>
+              <p className={classes.main_wrapper__chart__title_text_top}>
+                USD / RUB • CURRENCY
+              </p>
+              <p className={classes.main_wrapper__chart__title_text_bottom}>
+                US Dollar/Russian Ruble
+              </p>
+            </div>
+            <ButtonMui
+              padding='12px 24px'
+              text="Обменять"
+              color='white'
+              background='#363636'
+              hoverBackground='#363636'
+            />
+          </div>
+          <div className={classes.main_wrapper__chart}>
+            <div className={classes.main_wrapper__chart_price}>
+              <p className={classes.main_wrapper__chart_price_currency}>83,8750</p>
+              <p className={classes.main_wrapper__chart_price_percent}><img src={arrowUpMin} alt='Проценты'/>0,45 %</p>
+              <p className={classes.main_wrapper__chart_price_plus}>+0,3750 Today</p>
+            </div>
+
+
+          </div>
+          <Charts/>
+        </main>
+        <ProfileBar/>
+      </div>
+    </>
   );
 };
 
